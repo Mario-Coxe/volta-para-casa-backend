e4a928b140c43bdb5dbbae11fa577ed995451bc8
Mario-Coxe <mario.coxe@ideiasdinamicas.com>
2024-07-06 02:11:21 +0100

    feat:  migration and model for users

---
+++ database/migrations/1720227845830_users.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('full_name', 100).notNullable()
+      table.string('phone_number', 100).notNullable().unique()
+      table.string('password', 255).notNullable()
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
