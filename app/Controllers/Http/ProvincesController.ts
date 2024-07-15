f37f5ac92b772c6b887daba881e7b78717c78077
Mário Coxe <94186890+Mario-Coxe@users.noreply.github.com>
2024-07-15 14:43:48 +0100

    feat: ornament of the return of the provinces by the most recent

--- app/Controllers/Http/ProvincesController.ts
+++ app/Controllers/Http/ProvincesController.ts
@@ -3,9 +3,9 @@ import Province from 'App/Models/Province'
 
 export default class ProvincesController {
   public async index({ request, response }: HttpContextContract) {
-    const page = request.input('page', 1) 
-    const limit = request.input('limit', 10) 
-    const provinces = await Province.query().paginate(page, limit)
+    const page = request.input('page', 1)
+    const limit = request.input('limit', 10)
+    const provinces = await Province.query().orderBy('created_at', 'desc').paginate(page, limit)
     return response.json(provinces)
   }
 }
