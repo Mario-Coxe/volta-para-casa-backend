5a43bf322175a9495bd2f0efeef02e61d077a127
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-10-10 12:57:07 +0100

    fix: configuring the database so that the password and database come from the .env

--- config/database.ts
+++ config/database.ts
@@ -57,11 +57,9 @@ const databaseConfig: DatabaseConfig = {
       connection: {
         host: Env.get('DB_HOST', 'localhost'),
         port: Env.get('DB_PORT', '3306'),
-        user: Env.get('DB_USER', 'root'),
+        user: Env.get('MYSQL_USER'),
         password: Env.get('DB_PASSWORD'),
         database: Env.get('DB_DATABASE'),
-
-
       },
       debug: Env.get('DB_DEBUG', false),
     },
